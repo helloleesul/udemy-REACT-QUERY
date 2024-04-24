f3c2d594a4b424cba2b7ef1c950ce433298beab6
LEE SUL <suerish.e@gmail.com>
2024-04-24 21:40:47 +0900

    Add: useIsFetching을 사용하는 중앙 집중식 페칭 표시기(Indicator)

--- base-lazy-days/client/src/components/app/Loading.tsx
+++ base-lazy-days/client/src/components/app/Loading.tsx
@@ -1,8 +1,10 @@
 import { Spinner, Text } from "@chakra-ui/react";
+import { useIsFetching } from "@tanstack/react-query";
 
 export function Loading() {
   // will use React Query `useIsFetching` to determine whether or not to display
-  const isFetching = false; // for now, just don't display
+  const isFetching = useIsFetching();
+  // useIsFetching 현재 가져오는 쿼리가 있는지 여부를 나타내는 훅
   const display = isFetching ? "inherit" : "none";
 
   return (
